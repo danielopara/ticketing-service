d0e600eacbfa9dfade67a9f1d3723fd79e982bd4
Daniel Opara <oparadaniv@gmail.com>
2024-09-12 16:46:49 +0100

    findByToken

--- src/main/java/com/projects/Ticketing/repository/TrailRepository.java
+++ src/main/java/com/projects/Ticketing/repository/TrailRepository.java
@@ -4,6 +4,9 @@ import com.projects.Ticketing.model.LogInOutTrail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TrailRepository extends JpaRepository<LogInOutTrail, Long> {
+    Optional<LogInOutTrail> findByToken(String token);
 }
